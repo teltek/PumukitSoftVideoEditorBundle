7ad55a63b4379ca54c3134d28677164e6fc8b525
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-10-08 09:46:14 +0200

    Fix load routing by annotations

--- Controller/VideoController.php
+++ Controller/VideoController.php
@@ -8,6 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("admin/videoeditor")
+ */
 class VideoController extends AbstractController
 {
     /**
